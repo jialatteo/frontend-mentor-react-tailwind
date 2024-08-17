6e4aed3671a2ba81e7dfbdd06e0b72b01ca19389
Gerald Teo <geraldteo7@gmail.com>
2024-08-17 23:57:26 +0800

    Initial setup

---
+++ src/challenges/social-links-profile/SocialLinksProfile.js
@@ -0,0 +1,9 @@
+export default function SocialLinksProfile() {
+    return (
+        <h1 className="text-3xl bg-blue-100 font-bold underline">
+            <p> i am social links profile </p>
+            <p>fahjsfhj lsdak</p>
+        </h1>
+    )
+}
+
