9b36f89ffefe11475072161ddfe8a3e977a29bbd
Gerald Teo <geraldteo7@gmail.com>
2024-11-11 17:07:22 +0800

    Fix cart ui divider

--- src/challenges/product-list-with-cart/ProductListWithCart.js
+++ src/challenges/product-list-with-cart/ProductListWithCart.js
@@ -37,52 +37,51 @@ export default function ProductListWithCart() {
           <h2 className="mb-4 text-2xl font-bold text-product-list-with-cart-red">
             Your Cart (1)
           </h2>
-          <div className="divide-y-2">
-            {productsInCart.reduce((acc, dessert) => {
-              acc += dessert.quantity;
-              return acc;
-            }, 0) == 0 ? (
-              <div className="flex flex-col items-center justify-center">
-                <img src="product-list-with-cart/illustration-empty-cart.svg" />
-                <p className="mt-2 text-sm font-semibold text-product-list-with-cart-rose-500">
-                  Your added items will appear here
-                </p>
-              </div>
-            ) : (
-              <div>
-                {productsInCart
-                  .filter((product) => product.quantity != 0)
-                  .map((product) => (
-                    <Order
-                      name={product.name}
-                      quantity={product.quantity}
-                      price={
-                        data.find((dessert) => dessert.name == product.name)
-                          ?.price
-                      }
-                      setProductsInCart={setProductsInCart}
-                    />
-                  ))}
-              </div>
-            )}
-            <div className="flex items-center justify-between pt-6">
-              <p className="text-sm text-product-list-with-cart-rose-500">
-                Order Total:
-              </p>
-              <p className="text-2xl font-bold">
-                {productsInCart
-                  .reduce(
-                    (acc, product) =>
-                      acc +
-                      product.quantity *
-                        data.find((dessert) => dessert.name == product.name)
-                          ?.price,
-                    0,
-                  )
-                  .toFixed(2)}
+          {productsInCart.reduce((acc, dessert) => {
+            acc += dessert.quantity;
+            return acc;
+          }, 0) == 0 ? (
+            <div className="flex flex-col items-center justify-center">
+              <img src="product-list-with-cart/illustration-empty-cart.svg" />
+              <p className="mt-2 text-sm font-semibold text-product-list-with-cart-rose-500">
+                Your added items will appear here
               </p>
             </div>
-          </div>
+          ) : (
+            <div className="divide-y">
+              {productsInCart
+                .filter((product) => product.quantity != 0)
+                .map((product) => (
+                  <Order
+                    name={product.name}
+                    quantity={product.quantity}
+                    price={
+                      data.find((dessert) => dessert.name == product.name)
+                        ?.price
+                    }
+                    setProductsInCart={setProductsInCart}
+                  />
+                ))}
+              <div className="flex items-center justify-between pt-6">
+                <p className="text-sm text-product-list-with-cart-rose-500">
+                  Order Total:
+                </p>
+                <p className="text-2xl font-bold">
+                  $
+                  {productsInCart
+                    .reduce(
+                      (acc, product) =>
+                        acc +
+                        product.quantity *
+                          data.find((dessert) => dessert.name == product.name)
+                            ?.price,
+                      0,
+                    )
+                    .toFixed(2)}
+                </p>
+              </div>
+            </div>
+          )}
           <div className="my-6 flex items-center justify-center gap-2 bg-product-list-with-cart-rose-50 py-4">
             <img src="product-list-with-cart/icon-carbon-neutral.svg" />
             <p className="text-sm">
