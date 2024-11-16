b7d54d0b9ca84d91f69d9540a9d8f6c3c413f786
Gerald Teo <geraldteo7@gmail.com>
2024-11-17 00:24:45 +0800

    Convert all svg to react components

--- src/challenges/url-shortener/UrlShortener.js
+++ src/challenges/url-shortener/UrlShortener.js
@@ -1,3 +1,12 @@
+import BgBoostMobileSvg from "./images/BgBoostMobileSvg";
+import BgShortenDesktopSvg from "./images/BgShortenDesktopSvg";
+import IconTwitterSvg from "./images/IconTwitterSvg";
+
 export default function UrlShortener() {
-  return <p>hi</p>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <BgBoostMobileSvg />
+      <IconTwitterSvg />
+    </div>
+  );
 }
