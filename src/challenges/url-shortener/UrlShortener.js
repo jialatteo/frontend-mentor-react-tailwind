d5442722c14fb7b7305532ae813c475620616d49
Gerald Teo <geraldteo7@gmail.com>
2024-11-19 00:49:39 +0800

    Done with desktop and logic and error message display

--- src/challenges/url-shortener/UrlShortener.js
+++ src/challenges/url-shortener/UrlShortener.js
@@ -118,7 +118,7 @@ export default function UrlShortener() {
           >
             <input
               type="text"
-              className="ml-12 mr-6 h-14 w-full rounded-lg px-4 text-xl"
+              className={`ml-12 mr-6 h-14 w-full rounded-lg px-4 text-xl ${errors.inputLink ? "border-[3px] border-red-500 placeholder:text-red-500 focus:outline-red-500" : ""}`}
               placeholder="Shorten a link here..."
               {...register("inputLink")}
             />
@@ -127,7 +127,10 @@ export default function UrlShortener() {
                 {errors.inputLink.message}
               </p>
             )}
-            <button className="mr-12 flex w-48 items-center justify-center rounded-lg bg-url-shortener-cyan px-6 py-[15px] text-lg font-semibold text-white hover:bg-[#99E3E2] hover:text-opacity-95">
+            <button
+              className={` ${isLoading ? "hover:bg-url-shortener-cyan" : ""} mr-12 flex w-48 items-center justify-center rounded-lg bg-url-shortener-cyan px-6 py-[15px] text-lg font-semibold text-white hover:bg-[#99E3E2] hover:text-opacity-95`}
+              disabled={isLoading}
+            >
               {isLoading ? (
                 // prettier-ignore
                 <svg class="text-gray-300 animate-spin" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg" width="24" height="24"> <path d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762" stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" class="text-gray-900"> </path> </svg>
@@ -141,19 +144,21 @@ export default function UrlShortener() {
         <div className="mt-20"></div>
         {shortenedLinks.map((link) => (
           <div className="mb-3 flex w-[calc(100%-16rem)] items-center justify-between rounded-lg bg-white p-4 pl-8">
-            <div className="flex w-full justify-between">
-              <p className="text-xl font-medium">{link.original}</p>
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xl font-medium">
+              {link.original}
+            </p>
+            <div className="flex items-center justify-center">
               <p className="text-xl font-medium text-url-shortener-cyan">
                 {link.shortened}
               </p>
+              <button
+                type="button"
+                onClick={() => handleCopy(link)}
+                className={`ml-4 w-28 flex-shrink-0 rounded-md ${link.copied ? "bg-url-shortener-very-dark-blue" : "bg-url-shortener-cyan hover:bg-[#99E3E2]"} py-2 font-semibold text-white`}
+              >
+                {link.copied ? "Copied!" : "Copy"}
+              </button>
             </div>
-            <button
-              type="button"
-              onClick={() => handleCopy(link)}
-              className={`ml-4 w-28 rounded-md ${link.copied ? "bg-url-shortener-very-dark-blue" : "bg-url-shortener-cyan hover:bg-[#99E3E2]"} py-2 font-semibold text-white`}
-            >
-              {link.copied ? "Copied!" : "Copy"}
-            </button>
           </div>
         ))}
         <p className="my-4 mt-10 text-4xl font-bold">Advanced Statistics</p>
