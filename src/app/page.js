35fdefae049a5e102fc4f9a077836440f9c911af
Gerald Teo <geraldteo7@gmail.com>
2024-11-20 02:19:28 +0800

    Next JS

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the App</h1>
-      <p>Select a feature:</p>
-      <ul>
-        <li>
+    <div className="flex min-h-screen items-center justify-center">
+      <ul className="flex flex-col gap-8">
+        <li className="bg-slate-200">
           <a href="/product-list-with-cart">Product List with Cart</a>
         </li>
-        <li>
+        <li className="bg-slate-200">
           <a href="/mortgage-repayment-calculator">
             Mortgage Repayment Calculator
           </a>
         </li>
-        <li>
+        <li className="bg-slate-200">
           <a href="/url-shortener">URL Shortener</a>
         </li>
       </ul>
