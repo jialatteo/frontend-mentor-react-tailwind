24025ab50c951872f5dc612a897cf6a7690281c9
Gerald Teo <geraldteo7@gmail.com>
2024-11-20 13:09:22 +0800

    Create simple format to showcase frontend mentor projects

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,41 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <ul className="flex flex-col gap-8">
-        <li className="bg-slate-200">
-          <a href="/product-list-with-cart">Product List with Cart</a>
-        </li>
-        <li className="bg-slate-200">
-          <a href="/mortgage-repayment-calculator">
-            Mortgage Repayment Calculator
-          </a>
-        </li>
-        <li className="bg-slate-200">
-          <a href="/url-shortener">URL Shortener</a>
-        </li>
-      </ul>
+      <div className="grid grid-cols-3 gap-8">
+        <a
+          className="w-40 border border-blue-200 text-center text-xl hover:border-blue-600 hover:text-blue-600 hover:underline"
+          href="/product-list-with-cart"
+        >
+          <img
+            className="h-40 w-40"
+            src="/product-list-with-cart/active-states.jpg"
+            alt=""
+          />
+          Product List with Cart
+        </a>
+        <a
+          className="w-40 border border-blue-200 text-center text-xl hover:border-blue-600 hover:text-blue-600 hover:underline"
+          href="/mortgage-repayment-calculator"
+        >
+          <img
+            className="h-40 w-40"
+            src="/mortgage-repayment-calculator/active-states.jpg"
+            alt=""
+          />
+          Mortgage Repayment Calculator
+        </a>
+        <a
+          className="w-40 border border-blue-200 text-center text-xl hover:border-blue-600 hover:text-blue-600 hover:underline"
+          href="/url-shortener"
+        >
+          <img
+            className="h-40 w-40"
+            src="/url-shortener/desktop-active-states.jpg"
+            alt=""
+          />
+          URL Shortener
+        </a>
+      </div>
     </div>
   );
 }
