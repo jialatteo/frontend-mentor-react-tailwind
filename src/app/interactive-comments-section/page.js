f8748c9658c8e534898634ad39df61728aeddd61
Gerald Teo <geraldteo7@gmail.com>
2024-12-12 00:54:51 +0800

    Create OtherComment and SelfComment component

--- src/app/interactive-comments-section/page.js
+++ src/app/interactive-comments-section/page.js
@@ -20,35 +20,96 @@ export default function InteractiveCommentsSection() {
   };
 
   const selfComment = {
-    id: 4,
+    id: 2,
     content:
-      "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-    createdAt: "2 days ago",
-    score: 2,
-    replyingTo: "ramsesmiron",
+      "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+    createdAt: "2 weeks ago",
+    score: 5,
     user: {
       image: {
-        png: "interactive-comments-section/avatars/image-juliusomo.png",
-        webp: "interactive-comments-section/avatars/image-juliusomo.webp",
+        png: "interactive-comments-section/avatars/image-maxblagun.png",
+        webp: "interactive-comments-section/avatars/image-maxblagun.webp",
       },
-      username: "juliusomo",
+      username: "maxblagun",
     },
+    replies: [
+      {
+        id: 3,
+        content:
+          "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+        createdAt: "1 week ago",
+        score: 4,
+        replyingTo: "maxblagun",
+        user: {
+          image: {
+            png: "interactive-comments-section/avatars/image-ramsesmiron.png",
+            webp: "interactive-comments-section/avatars/image-ramsesmiron.webp",
+          },
+          username: "ramsesmiron",
+        },
+      },
+      {
+        id: 4,
+        content:
+          "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+        createdAt: "2 days ago",
+        score: 2,
+        replyingTo: "ramsesmiron",
+        user: {
+          image: {
+            png: "interactive-comments-section/avatars/image-juliusomo.png",
+            webp: "interactive-comments-section/avatars/image-juliusomo.webp",
+          },
+          username: "juliusomo",
+        },
+      },
+    ],
   };
-
   const otherComment = {
-    id: 1,
+    id: 2,
     content:
-      "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-    createdAt: "1 month ago",
-    score: 12,
+      "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+    createdAt: "2 weeks ago",
+    score: 5,
     user: {
       image: {
-        png: "interactive-comments-section/avatars/image-amyrobson.png",
-        webp: "interactive-comments-section/avatars/image-amyrobson.webp",
+        png: "interactive-comments-section/avatars/image-maxblagun.png",
+        webp: "interactive-comments-section/avatars/image-maxblagun.webp",
       },
-      username: "amyrobson",
+      username: "maxblagun",
     },
-    replies: [],
+    replies: [
+      {
+        id: 3,
+        content:
+          "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+        createdAt: "1 week ago",
+        score: 4,
+        replyingTo: "maxblagun",
+        user: {
+          image: {
+            png: "interactive-comments-section/avatars/image-ramsesmiron.png",
+            webp: "interactive-comments-section/avatars/image-ramsesmiron.webp",
+          },
+          username: "ramsesmiron",
+        },
+      },
+      {
+        id: 4,
+        content:
+          "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+        createdAt: "2 days ago",
+        score: 2,
+        replyingTo: "ramsesmiron",
+        user: {
+          image: {
+            png: "interactive-comments-section/avatars/image-juliusomo.png",
+            webp: "interactive-comments-section/avatars/image-juliusomo.webp",
+          },
+          username: "juliusomo",
+        },
+      },
+    ],
   };
   return (
     <div
