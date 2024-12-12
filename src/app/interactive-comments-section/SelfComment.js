7ced4368b1323637c2b4ace6c1af274f3ad31157
Gerald Teo <geraldteo7@gmail.com>
2024-12-13 01:24:08 +0800

    Fix issue with vertical score sizing

--- src/app/interactive-comments-section/SelfComment.js
+++ src/app/interactive-comments-section/SelfComment.js
@@ -10,19 +10,22 @@ export default function SelfComment({ currentUsername, comment }) {
     <div className="w-full max-w-[1600px]">
       <div className="rounded-lg bg-white p-4">
         <div className="flex">
-          <div className="bg-interactive-comments-section-very-light-gray mr-4 hidden min-w-[32px] flex-col items-center justify-start rounded-md py-2 sm:flex">
-            <button className="group px-2">
-              {/* prettier-ignore */}
-              <svg id="icon-plus" className="group-hover:fill-interactive-comments-section-moderate-blue fill-interactive-comments-section-light-grayish-blue" width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"/></svg>
-            </button>
-            <p className="text-interactive-comments-section-moderate-blue font-medium">
-              {comment.score}
-            </p>
-            <button className="group px-2">
-              {/* prettier-ignore */}
-              <svg id="icon-minus" className="group-hover:fill-interactive-comments-section-moderate-blue fill-interactive-comments-section-light-grayish-blue" width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"/></svg>
-            </button>
+          <div className="hidden sm:block">
+            <div className="bg-interactive-comments-section-very-light-gray mr-4 flex min-w-12 flex-col items-center justify-center gap-4 rounded-md py-4 pb-2 pt-4">
+              <button className="group">
+                {/* prettier-ignore */}
+                <svg id="icon-plus" className="group-hover:fill-interactive-comments-section-moderate-blue fill-interactive-comments-section-light-grayish-blue" width="11" height="11" xmlns="http://www.w3.org/2000/svg"><path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"/></svg>
+              </button>
+              <p className="text-interactive-comments-section-moderate-blue font-medium">
+                {comment.score}
+              </p>
+              <button className="group pb-3">
+                {/* prettier-ignore */}
+                <svg id="icon-minus" className="group-hover:fill-interactive-comments-section-moderate-blue fill-interactive-comments-section-light-grayish-blue" width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"/></svg>
+              </button>
+            </div>
           </div>
+
           <div className="flex-1">
             <div className="mb-4 flex items-center justify-between">
               <div className="flex items-center gap-5">
