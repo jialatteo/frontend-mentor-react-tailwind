e90c9202098a62a1f2f3d762772e269524825df1
Gerald Teo <geraldteo7@gmail.com>
2024-12-12 15:09:12 +0800

    Add reply text area

--- src/app/interactive-comments-section/OtherComment.js
+++ src/app/interactive-comments-section/OtherComment.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function OtherComment({ comment }) {
+  const [isReplying, setIsReplying] = useState(false);
+  const [replyContent, setReplyContent] = useState("");
+
   return (
     <div>
       <div className="rounded-lg bg-white p-4">
@@ -34,15 +39,55 @@ export default function OtherComment({ comment }) {
               <svg id="icon-minus" className="group-hover:fill-interactive-comments-section-moderate-blue fill-interactive-comments-section-light-grayish-blue" width="11" height="3" xmlns="http://www.w3.org/2000/svg"><path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"/></svg>
             </button>
           </div>
-          <button className="group flex items-center gap-2">
+          <button
+            disabled={isReplying}
+            onClick={() => setIsReplying(true)}
+            className="group flex items-center gap-2"
+          >
             {/* prettier-ignore */}
-            <svg id="icon-reply" className="group-hover:opacity-50" width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
-            <p className="text-interactive-comments-section-moderate-blue font-medium group-hover:opacity-50">
+            <svg id="icon-reply" className={`${isReplying ? "fill-interactive-comments-section-light-gray" : "group-hover:opacity-50 fill-interactive-comments-section-moderate-blue"}`} width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"/></svg>
+            <p
+              className={`${isReplying ? "text-interactive-comments-section-light-gray" : "text-interactive-comments-section-moderate-blue group-hover:opacity-50"} font-medium`}
+            >
               Reply
             </p>
           </button>
         </div>
       </div>
+      {isReplying && (
+        <div className="mt-3 rounded-lg bg-white p-4">
+          <textarea
+            onChange={(e) => setReplyContent(e.target.value)}
+            className="focus:outline-interactive-comments-section-moderate-blue border-interactive-comments-section-light-gray mb-2 h-28 w-full rounded-lg border px-4 py-2"
+          >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            voluptatibus iusto tempore tenetur fuga omnis modi odio quod illo
+            nostrum?
+          </textarea>
+          <div className="flex justify-end gap-3">
+            <button
+              onClick={() => {
+                setIsReplying(false);
+              }}
+              className="bg-interactive-comments-section-grayish-blue rounded-md px-4 py-3 hover:opacity-50"
+            >
+              <p className="font-medium text-white group-hover:opacity-50">
+                CANCEL
+              </p>
+            </button>
+            <button
+              onClick={() => {
+                setIsReplying(false);
+              }}
+              className="bg-interactive-comments-section-moderate-blue rounded-md px-4 py-3 hover:opacity-50"
+            >
+              <p className="font-medium text-white group-hover:opacity-50">
+                REPLY
+              </p>
+            </button>
+          </div>
+        </div>
+      )}
       {comment?.replies?.length > 0 && (
         <div className="mt-3 flex">
           <div className="bg-interactive-comments-section-light-gray mr-4 w-1 self-stretch" />
