d8a1c26dd1e1bdb9237af950fd9839aadd29fe7e
Gerald Teo <geraldteo7@gmail.com>
2024-12-26 01:33:22 +0800

    Done with thank you page

--- src/app/multi-step-form/page.js
+++ src/app/multi-step-form/page.js
@@ -3,13 +3,7 @@ import { Ubuntu } from "next/font/google";
 import { useState } from "react";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { get, useForm } from "react-hook-form";
-
-// fix the error message supposed to not take up space and be right top hand corner of text input
-// create thank you for last page. see if there is a isSubmitted in react hook form?
-// change the go next for step 4 to submit. create submit function
-// fix the stupid ecommerce 2 image carousel shit
-// start of the stupid calculator
+import { useForm } from "react-hook-form";
 
 const ubuntu = Ubuntu({
   weight: ["400", "500", "700"],
@@ -23,17 +17,10 @@ const validationSchemas = [
       .email("Invalid Email"),
     phoneNumber: Yup.string()
       .required("Phone number is required")
-      .matches(
-        /^[0-9]+$/,
-        "Phone number must only contain digits. No whitespaces.",
-      )
-      .test(
-        "is-valid-phone-number",
-        "Singapore phone number must be 8 digits",
-        (value) => {
-          return value && value.length === 8;
-        },
-      ),
+      .matches(/^[0-9]+$/, "Only digits, no whitespaces.")
+      .test("is-valid-phone-number", "Must be 8 digits", (value) => {
+        return value && value.length === 8;
+      }),
   }),
   Yup.object({
     plan: Yup.string()
@@ -126,8 +113,6 @@ export default function MultiStepForm() {
     console.log("data", data);
   };
 
-  console.log("isSubmitted", isSubmitted);
-
   const isMonthlyBilling = watch("isMonthlyBilling");
 
   return (
@@ -185,7 +170,7 @@ export default function MultiStepForm() {
         </div>
 
         <div className="min[800px]:px-6 relative -top-14 mx-4 flex flex-1 flex-col justify-between rounded-lg bg-white p-6 sm:top-0 sm:px-0 sm:pb-0 min-[800px]:mx-12">
-          {activeStep === 0 && (
+          {!isSubmitted && activeStep === 0 && (
             <div>
               <h1 className="mb-1 text-2xl font-bold text-multi-step-form-marine-blue sm:text-3xl">
                 Personal info
@@ -193,7 +178,7 @@ export default function MultiStepForm() {
               <h2 className="mb-4 text-multi-step-form-cool-gray sm:mb-6">
                 Please provide your name, email address and phone number.
               </h2>
-              <div className="flex flex-col gap-3 text-multi-step-form-marine-blue sm:gap-6">
+              <div className="relative flex flex-col gap-3 text-multi-step-form-marine-blue sm:gap-6">
                 <label
                   htmlFor="name"
                   className="flex flex-col gap-1 text-sm font-medium"
@@ -207,7 +192,7 @@ export default function MultiStepForm() {
                     {...register("name")}
                   />
                   {errors?.name && (
-                    <p className="mt-1 text-sm text-red-500">
+                    <p className="absolute right-0 text-sm text-red-500">
                       {errors?.name?.message}
                     </p>
                   )}
@@ -226,7 +211,7 @@ export default function MultiStepForm() {
                     {...register("emailAddress")}
                   />
                   {errors?.emailAddress && (
-                    <p className="mt-1 text-sm text-red-500">
+                    <p className="absolute right-0 text-sm text-red-500">
                       {errors?.emailAddress?.message}
                     </p>
                   )}
@@ -245,7 +230,7 @@ export default function MultiStepForm() {
                     {...register("phoneNumber")}
                   />
                   {errors?.phoneNumber && (
-                    <p className="mt-1 text-sm text-red-500">
+                    <p className="absolute right-0 text-sm text-red-500">
                       {errors?.phoneNumber?.message}
                     </p>
                   )}
@@ -254,7 +239,7 @@ export default function MultiStepForm() {
             </div>
           )}
 
-          {activeStep === 1 && (
+          {!isSubmitted && activeStep === 1 && (
             <div>
               <h1 className="mb-1 text-2xl font-bold text-multi-step-form-marine-blue sm:text-3xl">
                 Select your plan
@@ -381,7 +366,7 @@ export default function MultiStepForm() {
             </div>
           )}
 
-          {activeStep === 2 && (
+          {!isSubmitted && activeStep === 2 && (
             <div>
               <h1 className="mb-1 text-2xl font-bold text-multi-step-form-marine-blue sm:text-3xl">
                 Pick add-ons
@@ -508,7 +493,7 @@ export default function MultiStepForm() {
             </div>
           )}
 
-          {activeStep === 3 && (
+          {!isSubmitted && activeStep === 3 && (
             <div>
               <h1 className="mb-1 text-2xl font-bold text-multi-step-form-marine-blue sm:text-3xl">
                 Finishing up
@@ -591,60 +576,81 @@ export default function MultiStepForm() {
             </div>
           )}
 
-          <div className="hidden self-stretch sm:flex">
-            <button
-              onClick={handleBack}
-              className="m-2 mr-auto rounded bg-transparent px-3 py-2 text-sm font-medium text-multi-step-form-cool-gray hover:text-multi-step-form-marine-blue disabled:hidden"
-              disabled={activeStep === 0}
-            >
-              Go Back
-            </button>
-            {activeStep === 3 ? (
+          {isSubmitted && (
+            <div className="flex flex-1 items-center justify-center">
+              <div className="flex flex-col items-center justify-center gap-4">
+                {/* prettier-ignore */}
+                <svg id="icon-thank-you" xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 80 80"><g fill="none"><circle cx="40" cy="40" r="40" fill="#F9818E"/><path fill="#E96170" d="M48.464 79.167c.768-.15 1.53-.321 2.288-.515a40.04 40.04 0 0 0 3.794-1.266 40.043 40.043 0 0 0 3.657-1.63 40.046 40.046 0 0 0 12.463-9.898A40.063 40.063 0 0 0 78.3 51.89c.338-1.117.627-2.249.867-3.391L55.374 24.698a21.6 21.6 0 0 0-15.332-6.365 21.629 21.629 0 0 0-15.344 6.365c-8.486 8.489-8.486 22.205 0 30.694l23.766 23.775Z"/><path fill="#FFF" d="M40.003 18.333a21.58 21.58 0 0 1 15.31 6.351c8.471 8.471 8.471 22.158 0 30.63-8.47 8.47-22.156 8.47-30.627 0-8.47-8.472-8.47-22.159 0-30.63a21.594 21.594 0 0 1 15.317-6.35Zm9.865 15c-.316.028-.622.15-.872.344l-12.168 9.13-5.641-5.642c-1.224-1.275-3.63 1.132-2.356 2.356l6.663 6.663c.56.56 1.539.63 2.173.156l13.326-9.995c1.122-.816.43-2.993-.956-3.013a1.666 1.666 0 0 0-.17 0Z"/></g></svg>
+                <h1 className="mt-4 text-3xl font-bold text-multi-step-form-marine-blue">
+                  Thank you!
+                </h1>
+                <p className="px-6 text-center text-multi-step-form-cool-gray">
+                  Thanks for confirming your subscription! We hope you have fun
+                  using our platform. If you need support, please feel free to
+                  email us at support@loremgaming.com
+                </p>
+              </div>
+            </div>
+          )}
+
+          {!isSubmitted && (
+            <div className="hidden self-stretch sm:flex">
               <button
-                onClick={handleSubmit(onSubmit)}
-                className="m-2 ml-auto rounded bg-multi-step-form-purplish-blue px-6 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
-                disabled={!isValid}
+                onClick={handleBack}
+                className="m-2 mr-auto rounded bg-transparent px-3 py-2 text-sm font-medium text-multi-step-form-cool-gray hover:text-multi-step-form-marine-blue disabled:hidden"
+                disabled={activeStep === 0}
               >
-                Submit
+                Go Back
               </button>
-            ) : (
-              <button
-                onClick={handleNext}
-                className="m-2 ml-auto rounded bg-multi-step-form-marine-blue px-3 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
-                disabled={!isValid || activeStep === 3}
-              >
-                Next step
-              </button>
-            )}
-          </div>
+              {activeStep === 3 ? (
+                <button
+                  onClick={handleSubmit(onSubmit)}
+                  className="m-2 ml-auto rounded bg-multi-step-form-purplish-blue px-6 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
+                  disabled={!isValid}
+                >
+                  Submit
+                </button>
+              ) : (
+                <button
+                  onClick={handleNext}
+                  className="m-2 ml-auto rounded bg-multi-step-form-marine-blue px-3 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
+                  disabled={!isValid || activeStep === 3}
+                >
+                  Next step
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
-      <div className="mb-8 flex self-stretch sm:hidden">
-        <button
-          onClick={handleBack}
-          className="m-2 mr-auto rounded bg-transparent px-3 py-2 text-sm font-medium text-multi-step-form-cool-gray hover:text-multi-step-form-marine-blue disabled:hidden"
-          disabled={activeStep === 0}
-        >
-          Go Back
-        </button>
-        {activeStep === 3 ? (
+      {!isSubmitted && (
+        <div className="mb-8 flex self-stretch sm:hidden">
           <button
-            onClick={handleSubmit(onSubmit)}
-            className="m-2 ml-auto rounded bg-multi-step-form-purplish-blue px-6 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
-            disabled={!isValid}
+            onClick={handleBack}
+            className="m-2 mr-auto rounded bg-transparent px-3 py-2 text-sm font-medium text-multi-step-form-cool-gray hover:text-multi-step-form-marine-blue disabled:hidden"
+            disabled={activeStep === 0}
           >
-            Submit
+            Go Back
           </button>
-        ) : (
-          <button
-            onClick={handleNext}
-            className="m-2 ml-auto rounded bg-multi-step-form-marine-blue px-3 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
-            disabled={!isValid || activeStep === 3}
-          >
-            Next step
-          </button>
-        )}
-      </div>
+          {activeStep === 3 ? (
+            <button
+              onClick={handleSubmit(onSubmit)}
+              className="m-2 ml-auto rounded bg-multi-step-form-purplish-blue px-6 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
+              disabled={!isValid}
+            >
+              Submit
+            </button>
+          ) : (
+            <button
+              onClick={handleNext}
+              className="m-2 ml-auto rounded bg-multi-step-form-marine-blue px-3 py-2 text-sm text-white hover:bg-opacity-80 disabled:cursor-not-allowed disabled:bg-opacity-15"
+              disabled={!isValid || activeStep === 3}
+            >
+              Next step
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
